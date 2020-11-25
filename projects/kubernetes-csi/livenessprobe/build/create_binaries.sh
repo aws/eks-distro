aac016b0f5b6b3d0895c74461f5a3c784fcaec9b
Abhay Krishna Arunachalam <arnchlm@amazon.com>
2020-11-24 18:35:04 -0800

    Add build steps for Kubernetes CSI projects

---
+++ projects/kubernetes-csi/livenessprobe/build/create_binaries.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -x
+set -o errexit
+set -o nounset
+set -o pipefail
+
+REPO="$1"
+CLONE_URL="$2"
+TAG="$3"
+BIN_ROOT="_output/bin"
+BIN_PATH=$BIN_ROOT/$REPO
+
+readonly SUPPORTED_PLATFORMS=(
+  linux/amd64
+  linux/arm64
+)
+
+function build::livenessprobe::binaries(){
+  mkdir -p $BIN_PATH
+  git clone $CLONE_URL $REPO
+  cd $REPO
+  git checkout $TAG
+  for platform in "${SUPPORTED_PLATFORMS[@]}";
+  do
+    OS="$(cut -d '/' -f1 <<< ${platform})"
+    ARCH="$(cut -d '/' -f2 <<< ${platform})"
+    make BUILD_PLATFORMS="$OS $ARCH"
+    mkdir -p ../${BIN_PATH}/${OS}-${ARCH}
+    mv bin/* ../${BIN_PATH}/${OS}-${ARCH}
+    make clean
+  done
+  cd ..
+  rm -rf $REPO
+}
+
+build::livenessprobe::binaries
