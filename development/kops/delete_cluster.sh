0acfbb8dc6fb784137170d4819b5bdcc44f3ae6a
Terry Howe <tlhowe@amazon.com>
2020-12-10 12:02:54 -0700

    Add delete cluster script

---
+++ development/kops/delete_cluster.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Copyright 2020 Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+export KOPS_STATE_STORE=${1:-${KOPS_STATE_STORE}}
+unset KOPS_CLUSTER_NAME
+BUCKET_NAME=$(echo "${KOPS_STATE_STORE}" | sed -e 's,s3://,,g')
+echo "Deleting cluster $KOPS_STATE_STORE"
+kops get cluster --state "${KOPS_STATE_STORE}" |
+  tail -n +2 |
+  cut -f1 -d '  ' |
+  while read NAME
+  do
+    set -x
+    kops delete cluster --state "${KOPS_STATE_STORE}" --name "${NAME}" --yes || true
+    set +x
+  done
+set -x
+aws s3 rm --recursive "${KOPS_STATE_STORE}" || true
+aws s3api delete-bucket --bucket "${BUCKET_NAME}"
