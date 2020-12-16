18a93e347eedcfaf123b3a48a29af9f81c5e95c1
Jackson West <jgw@amazon.com>
2020-12-16 12:20:33 -0600

    Fix version file and checksum creation for new kubernetes versions

--- projects/kubernetes/kubernetes/build/create_version_file.sh
+++ projects/kubernetes/kubernetes/build/create_version_file.sh
@@ -22,8 +22,9 @@ RELEASE_BRANCH=$2
 
 MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 source "${MAKE_ROOT}/build/lib/init.sh"
-if [ ! -d ${MAKE_ROOT}/_bin ] ;  then
-    echo "${MAKE_ROOT}/_bin not present! Run 'make binaries'"
+BIN_DIR="${OUTPUT_DIR}/${RELEASE_BRANCH}/bin"
+if [ ! -d ${BIN_DIR} ] ;  then
+    echo "${BIN_DIR} not present! Run 'make binaries'"
     exit 1
 fi
 
@@ -31,4 +32,4 @@ VERSION_FILE="${MAKE_ROOT}/${RELEASE_BRANCH}/KUBE_GIT_VERSION_FILE"
 rm -f $VERSION_FILE
 touch $VERSION_FILE
 RELEASE_FILE="${MAKE_ROOT}/${RELEASE_BRANCH}/RELEASE"
-build::version::create_env_file "$TAG" "$VERSION_FILE" "$RELEASE_FILE" "kubernetes"
+build::version::create_env_file "$TAG" "$VERSION_FILE" "$RELEASE_FILE" "kubernetes" "$RELEASE_BRANCH"
