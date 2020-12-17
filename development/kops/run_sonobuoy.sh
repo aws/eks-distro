6dc35b67b774b4a4eafa8f27f0b83178592ba981
Terry Howe <tlhowe@amazon.com>
2020-12-17 02:31:05 +0000

    Add set -x to run_sonobuoy

--- development/kops/run_sonobuoy.sh
+++ development/kops/run_sonobuoy.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
+set -exo pipefail
 
 export KOPS_STATE_STORE=${1:-${KOPS_STATE_STORE}}
 if [ -z "${KOPS_STATE_STORE}" ]
