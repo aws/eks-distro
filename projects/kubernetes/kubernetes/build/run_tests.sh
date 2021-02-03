14d9b67f2f870e756b53653057a1c766140141cf
Jackson West <jgw@amazon.com>
2021-02-03 17:01:53 -0600

    fix kubernetes patches to enable make test in builds (#124)

--- projects/kubernetes/kubernetes/build/run_tests.sh
+++ projects/kubernetes/kubernetes/build/run_tests.sh
@@ -20,20 +20,30 @@ set -o nounset
 set -o pipefail
 
 MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+source "${MAKE_ROOT}/build/lib/init.sh"
+source "${MAKE_ROOT}/../../../build/lib/common.sh"
 
 export ARTIFACTS=${ARTIFACTS:-"./_artifacts"}
 export KUBE_JUNIT_REPORT_DIR="${ARTIFACTS}"
 export KUBE_KEEP_VERBOSE_TEST_OUTPUT=y
 export LOG_LEVEL=4
+export KUBE_TIMEOUT=${KUBE_TIMEOUT:-"--timeout=600s"}
+
+build::binaries::use_go_version_k8s "$RELEASE_BRANCH"
+
+go get gotest.tools/gotestsum
 
 cd $MAKE_ROOT/kubernetes
 
 # Install etcd for tests
 ./hack/install-etcd.sh
 
-# Install gotestsum which is used to get the junit output
-# TODO: fails through athens proxy on the same dep as external-snapshotter
-GOPROXY=direct go get gotest.tools/gotestsum
-
-# Add gopath and etcd bin paths for use by unit test  
-PATH="${GOPATH}/bin:${MAKE_ROOT}/kubernetes/third_party/etcd:${PATH}" make test KUBE_TIMEOUT=--timeout=600s
+MAX_RETRIES=3
+# There are flakes in upstream tests, the process also caches passing results
+# so on rerun it only runs the tests which failed
+for i in $(seq 1 $MAX_RETRIES); 
+do 
+  [ $i -gt 1 ] && sleep 5
+  PATH="${GOPATH}/bin:${MAKE_ROOT}/kubernetes/third_party/etcd:${PATH}" make test && ret=0 && break || ret=$?; 
+done
+exit $ret
