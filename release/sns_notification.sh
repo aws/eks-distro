f55f9135a9aa52d2b2acd25e16046c7108f866da
Kirsten Schumy <ksschumy@amazon.com>
2021-03-17 14:45:39 -0700

    Refactor and handle errors

--- release/sns_notification.sh
+++ release/sns_notification.sh
@@ -17,51 +17,59 @@
 # HOW TO USE
 # - Prerequisites
 #   + The machine running the script must have the credentials needed to use your AWS account.
+#   + An intended release branch, which you must provide for the required argument RELEASE_BRANCH. This value must be in
+#     the expected format of "X-XX" (e.g. "1-19").
 #   + An SNS topic you wish publish the notification to. The default topic name is the value assigned to SNS_TOPIC_NAME,
 #     but you can override this if you choose. The topic must already exist, and you must have permission to list topics
-#     and publish to the intended topic. See SNS doc: https://docs.aws.amazon.com/sns/latest/dg/sns-configuring.html
-#   + A directory under NOTIFICATION_BODY_FILE_PATH that has a name that indicates what release branch you're
-#     publishing the notification about (e.g. eks-distro/tree/main/release/1-19). You must provided the directory name
-#     when running the script.
-#   + A file named 'RELEASE' in the above-mentioned release branch directory. This file must contain only the release
+#     and publish to the intended topic. Presumably, you should also have at least one subscriber. See official SNS docs:
+#     https://docs.aws.amazon.com/sns/latest/dg/sns-configuring.html
+#   + File paths must exist as expected, especially those that include directories named RELEASE_BRANCH.
+#   + A file named 'RELEASE' found at the script-determined RELEASE_NUMBER_PATH. This file must contain only the release
 #     number (e.g. "2" for 1.19.2).
-#   + A file named release-announcement.txt also in the above-mentioned release branch directory. This file must contain
+#   + A file named release-announcement.txt found at the script-determined NOTIFICATION_BODY_PATH. This file must contain
 #     the intended body of the SNS message.
-# - Run ./sns_notification.sh <release branch directory name> <OPTIONAL:SNS topic name>,
-#   e.g ./sns_notification.sh 1-19 my-sns-topic
-# - When prompted to confirm that you wish to publish, enter the appropriate response to do so. All ofter response exit.
-# - If the response received from SNS did not indicate there was an error, you will be provided the SNS MessageID.
+# - Run ./sns_notification.sh <release branch> <OPTIONAL:SNS topic name> (e.g. $ ./sns_notification.sh 1-19 my-sns-topic)
+# - When prompted to confirm that you wish to publish, review the provided notification preview, as this is your only
+#   chance to check what is about to be sent to all subscribers. If all the information looks correct and you wish to
+#   publish to the SNS topic, enter the requested response to confirm. All non-matching input results in exiting without
+#   sending the notification.
+# - If the response received from SNS did not indicate there was an error, you will be provided the MessageID.
 
-RELEASE_BRANCH="${1?First required argument is release branch, e.g. 1-19}"
-SNS_TOPIC_NAME="${2:-"eks-distro-updates"}"
+ANNOUNCEMNT_FILE="${1?First required argument is release branch, e.g. 1-19}"
+SNS_TOPIC_ARN="${2?Second required argument is SNS topic ARN}"
 
-# Get release directory
-RELEASE_BRANCH_DIRECTORY="$(git rev-parse --show-toplevel)/release/${RELEASE_BRANCH}"
+BASE_DIRECTORY="$(git rev-parse --show-toplevel)"
 
-# Get release notification body
-NOTIFICATION_BODY_FILE_PATH="${RELEASE_BRANCH_DIRECTORY}/release-announcement.txt"
-if ! [ -s "$NOTIFICATION_BODY_FILE_PATH" ]; then
-  echo "Non-empty text for notification message body expected at ${NOTIFICATION_BODY_FILE_PATH}"
+# Prints error message about failing to publish notification and then exits. Intended for when it is certain that the
+# notification was not published. The 1st argument should be details about what went wrong.
+fail_without_sending() {
+  echo "Failed to publish release notification: ${1}"
   exit 1
+}
+
+# Get release number
+RELEASE_NUMBER_PATH="${BASE_DIRECTORY}/release/${RELEASE_BRANCH}/RELEASE"
+if ! [ -s "$RELEASE_NUMBER_PATH" ]; then
+ fail_without_sending "Release number expected at ${RELEASE_NUMBER_PATH}"
 fi
-NOTIFICATION_BODY=$(<"${NOTIFICATION_BODY_FILE_PATH}")
+RELEASE_NUMBER=$(<"${RELEASE_NUMBER_PATH}")
 
 # Get release notification subject
-RELEASE_NUMBER_PATH="${RELEASE_BRANCH_DIRECTORY}/RELEASE"
-if ! [ -s "$RELEASE_NUMBER_PATH" ]; then
-  echo "Release number expected at ${RELEASE_NUMBER_PATH}"
-  exit 1
+NOTIFICATION_SUBJECT="EKS-D v${RELEASE_BRANCH/-/.}.${RELEASE_NUMBER} Release" # e.g. "EKS-D v1.19.2 Release"
+
+# Get release notification body
+NOTIFICATION_BODY_PATH="${BASE_DIRECTORY}/docs/contents/releases/${RELEASE_BRANCH}/${RELEASE_NUMBER}/release-announcement.txt"
+if ! [ -s "$NOTIFICATION_BODY_PATH" ]; then
+  fail_without_sending "Non-empty text for notification body expected at ${NOTIFICATION_BODY_PATH}"
 fi
-NOTIFICATION_SUBJECT="EKS-D v${RELEASE_BRANCH/-/.}.$(<"${RELEASE_NUMBER_PATH}") Release" # e.g. EKS-D v1.19.0 Release
+NOTIFICATION_BODY=$(<"${NOTIFICATION_BODY_PATH}")
 
 ## Get SNS topic ARN
 SNS_TOPIC_ARN=$(aws sns list-topics --query "Topics[?ends_with(TopicArn,':$SNS_TOPIC_NAME')].TopicArn | [0]" --output text)
-if [ "$SNS_TOPIC_ARN" == "None" ]; then
-  echo "Failed to send SNS notification because unable to find SNS topic'${SNS_TOPIC_NAME}'"
-  exit 1
-elif [ -z "$SNS_TOPIC_ARN" ]; then
-  echo "Failed to send SNS notification due to error when trying to get the SNS topic ARN for topic '${SNS_TOPIC_NAME}'"
-  exit 1
+if [ -z "$SNS_TOPIC_ARN" ]; then
+  fail_without_sending "Encountered error when trying to get the ARN for SNS topic '${SNS_TOPIC_NAME}'"
+elif [ "$SNS_TOPIC_ARN" == "None" ]; then
+  fail_without_sending "Unable to find SNS topic '${SNS_TOPIC_NAME}'"
 fi
 
 # Confirm release notification details
@@ -92,7 +100,7 @@ echo
 if [ "$SNS_MESSAGE_ID" ]; then
   echo "Release notification published with SNS MessageId $SNS_MESSAGE_ID"
 else
-  echo "Revived unexpected response while publishing to SNS topic. An error may have occurred, and the notification \
+  echo "Received unexpected response while publishing to SNS topic. An error may have occurred, and the notification \
 may not have not have been published"
   exit 1
 fi
