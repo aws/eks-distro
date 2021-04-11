8058cab78347eb6a14428531e318dbda3260f93f
Abhay Krishna Arunachalam <arnchlm@amazon.com>
2021-04-11 16:50:00 +0530

    Add OCI image tarballs for coredns and etcd

---
+++ projects/coredns/coredns/build/create_image_tars.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# Copyright 2020 Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -x
+set -o errexit
+set -o nounset
+set -o pipefail
+
+BASE_IMAGE="$1"
+RELEASE_BRANCH="$2"
+IMAGE="$3"
+
+readonly SUPPORTED_PLATFORMS=(
+  linux/amd64
+  linux/arm64
+)
+
+MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+source "${MAKE_ROOT}/../../../build/lib/common.sh"
+
+function build::coredns::image_tars(){
+  for platform in "${SUPPORTED_PLATFORMS[@]}";
+  do
+      OS="$(cut -d '/' -f1 <<< ${platform})"
+      ARCH="$(cut -d '/' -f2 <<< ${platform})"
+      buildctl \
+		      build \
+		      --frontend dockerfile.v0 \
+		      --opt platform=${platform} \
+		      --opt build-arg:BASE_IMAGE=${BASE_IMAGE} \
+          --opt build-arg:RELEASE_BRANCH=${RELEASE_BRANCH} \
+		      --local dockerfile=./docker/linux \
+		      --local context=. \
+		      --output type=oci,oci-mediatypes=true,name=${IMAGE},dest=${MAKE_ROOT}/_output/bin/coredns/${OS}-${ARCH}/coredns.tar
+  done
+}
+
+build::coredns::image_tars
