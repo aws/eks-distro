8b09e8ecfdcc5fb68cc832327ff1e8246d69c4d8
Kirsten Schumy <ksschumy@amazon.com>
2021-04-23 19:33:37 -0700

    Changed versions for 1.20-1

--- development/pull-all.sh
+++ development/pull-all.sh
@@ -13,30 +13,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# TODO: UPDATE ECR FOR EACH
 set -e
 set -x
 
 RELEASE_BRANCH=1-20
 RELEASE=1
 RELEASE_TAG="eks-${RELEASE_BRANCH}-${RELEASE}"
+KUBERNETES_VERSION=v1.20.4
 
-docker pull public.ecr.aws/eks-distro/kubernetes/pause:v1.18.9-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes/pause:${KUBERNETES_VERSION}-${RELEASE_TAG}  # TODO: want the hard coded version?
 docker pull public.ecr.aws/eks-distro/kubernetes-csi/external-provisioner:v2.0.3-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes/go-runner:v0.4.2-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes-csi/external-snapshotter/csi-snapshotter:v3.0.2-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes-csi/external-snapshotter/snapshot-validation-webhook:v3.0.2-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes-csi/livenessprobe:v2.1.0-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes-csi/external-snapshotter/snapshot-controller:v3.0.2-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes/kube-scheduler:v1.18.9-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes-csi/external-attacher:v3.0.1-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes/kube-proxy-base:v0.4.2-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes/go-runner:v0.4.2-${RELEASE_TAG} # TODO: confirm this is correct
+docker pull public.ecr.aws/eks-distro/kubernetes-csi/external-snapshotter/csi-snapshotter:v3.0.3-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes-csi/external-snapshotter/snapshot-validation-webhook:v3.0.3-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes-csi/livenessprobe:v2.2.0-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes-csi/external-snapshotter/snapshot-controller:v3.0.3-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes/kube-scheduler:${KUBERNETES_VERSION}-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes-csi/external-attacher:v3.1.0-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes/kube-proxy-base:v0.4.2-${RELEASE_TAG} # TODO: confirm this is correct
 docker pull public.ecr.aws/eks-distro/kubernetes-sigs/aws-iam-authenticator:v0.5.2-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes-sigs/metrics-server:v0.4.0-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/etcd-io/etcd:v3.4.14-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes/kube-proxy:v1.18.9-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/coredns/coredns:v1.7.0-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes-csi/node-driver-registrar:v2.0.1-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes/kube-controller-manager:v1.18.9-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes-sigs/metrics-server:v0.4.3-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/etcd-io/etcd:v3.4.15-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes/kube-proxy:${KUBERNETES_VERSION}-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/coredns/coredns:v1.8.3-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes-csi/node-driver-registrar:v2.1.0-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes/kube-controller-manager:${KUBERNETES_VERSION}-${RELEASE_TAG}
 docker pull public.ecr.aws/eks-distro/kubernetes-csi/external-resizer:v1.0.1-${RELEASE_TAG}
-docker pull public.ecr.aws/eks-distro/kubernetes/kube-apiserver:v1.18.9-${RELEASE_TAG}
+docker pull public.ecr.aws/eks-distro/kubernetes/kube-apiserver:${KUBERNETES_VERSION}-${RELEASE_TAG}
