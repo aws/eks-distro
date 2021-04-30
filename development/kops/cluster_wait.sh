6be986373f416da446678e99772facadf333b26e
Jackson West <jaxesn@gmail.com>
2021-04-30 13:12:15 -0500

    overwrite clusterrole for coredns to have endpointslices perms (#337)

--- development/kops/cluster_wait.sh
+++ development/kops/cluster_wait.sh
@@ -35,5 +35,23 @@ do
     echo 'Waiting for cluster to come up...'
 done
 
+# In 1-20 since we are using coredns 1.8.3 which now watches endpointslices
+# instead of endpoints, we need to add additional permissions that kops
+# does not currently add since it still supports coredns 1.7.x
+if [ "${RELEASE_BRANCH}" == "1-20" ]; then
+while ! kubectl --context $KOPS_CLUSTER_NAME apply -f ./core_dns_cluster_role.yaml
+do
+    sleep 5
+    COUNT=$(expr $COUNT + 1)
+    if [ $COUNT -gt 120 ]
+    then
+        echo "Failed to configure coredns clusterrole"
+        exit 1
+    fi
+    echo 'Waiting for cluster to come up...'
+done
+fi
+
+
 set -x
 ${KOPS} validate cluster --wait 15m
