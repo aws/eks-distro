e249d67bcc9c294c698da0eed94eb7936c563dbc
Abhay Krishna <arnchlm@amazon.com>
2021-06-24 19:04:31 -0700

    Generate attribution file in presubmits as dry-run (#408)

--- projects/etcd-io/etcd/build/create_tarballs.sh
+++ projects/etcd-io/etcd/build/create_tarballs.sh
@@ -46,8 +46,6 @@ function build::etcd::tarball() {
     cp "$RELEASE_BRANCH"/ATTRIBUTION.txt $BIN_ROOT/$REPO/${OS}-${ARCH}/
     build::common::create_tarball  "${TAR_PATH}/${TAR_FILE}" "${BIN_ROOT}/${REPO}" "$OS"-"$ARCH"
   done
-  rm -rf "$BIN_ROOT"
-  rm -rf "$LICENSES_PATH"
 }
 
 build::etcd::tarball
