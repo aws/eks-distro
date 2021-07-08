05d3f9fe98376677cdbf5374fa9d006148f708d5
Abhisek Banerjee <68349259+bnrjee@users.noreply.github.com>
2021-07-08 15:45:34 -0700

    Kops should not modify iam roles (#431)

--- development/kops/set_nodeport_access.sh
+++ development/kops/set_nodeport_access.sh
@@ -25,4 +25,4 @@ $PREFLIGHT_CHECK_PASSED || exit 1
 export KOPS_FEATURE_FLAGS=SpecOverrideFlag
 ${KOPS} set cluster "${KOPS_CLUSTER_NAME}" 'cluster.spec.nodePortAccess=0.0.0.0/0'
 
-${KOPS} update cluster --yes
+${KOPS} update cluster --yes --lifecycle-overrides IAMRole=ExistsAndWarnIfChanges,IAMRolePolicy=ExistsAndWarnIfChanges,IAMInstanceProfileRole=ExistsAndWarnIfChanges
