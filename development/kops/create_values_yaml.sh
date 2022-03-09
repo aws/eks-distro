15eb0c1a93d1021aec9a9162f15b7ed1ef301d15
Jackson West <jaxesn@gmail.com>
2022-03-09 11:44:57 -0600

    run 1-21 + 1-22 arm kops cluster as ipv6 (#770)
    * run 1-21 + 1-22 arm kops cluster as ipv6
    
    * Update set_environment.sh

--- development/kops/create_values_yaml.sh
+++ development/kops/create_values_yaml.sh
@@ -67,7 +67,7 @@ controlPlaneInstanceProfileArn: $CONTROL_PLANE_INSTANCE_PROFILE
 nodeInstanceProfileArn: $NODE_INSTANCE_PROFILE
 instanceType: $NODE_INSTANCE_TYPE
 architecture: $NODE_ARCHITECTURE
-ipv6: false
+ipv6: $IPV6
 pause:
 $(get_container_yaml kubernetes/pause $RELEASE)
 kube_apiserver:
