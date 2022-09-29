af0d4700ff8d98c49d71dca9075d45c105ec5d34
Cameron Rozean <rcrozean@amazon.com>
2022-09-29 12:47:25 -0700

    fix merge issues

--- cmd/release/docs/new_docs/constants.go
+++
@@ -1,8 +0,0 @@
-package new_docs
-
-const (
-	SearchLastDocRelease = "label:release,documentation is:closed is:pr"
-	SearchPatchesSinceLDR = "q="
-	SearchBaseImgChangesSinceLDR = "q="
-	SearchVerChangesSinceLDR = "q="
-)
