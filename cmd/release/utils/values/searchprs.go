bb1448c71b6af778785c6a730701edda16602c35
rcrozean <61116778+rcrozean@users.noreply.github.com>
2022-10-04 13:44:41 -0500

    Add automation for gathering info for the CHANGELOGs (#1252)
    * Merge upstream
    
    * Major refactor of release make commands
    
    * Fixed github release
    
    * Add automation for adding the additional information to the Manifest info
    
    * merge fixes and fix comment typo
    
    * add changelog initial github pulls
    
    * test
    
    * add the stdout for the cmds for better debugging issues
    
    * pull specific branch versions prs
    
    * added search logic for the tags
    
    * add the additional labels for autolabeling
    
    * change to look for merged files after merge date
    
    * update dependency labeler
    
    * fix merge issues
    
    * remove move files and residual merge issues
    
    * fix residual merge issues
    
    * update labeler to accurately label GIT_TAG changes
    
    * consolidate pr appending functions and queries
    
    * handle errors
    
    Co-authored-by: Cameron Rozean <rcrozean@amazon.com>
    Co-authored-by: Kirsten Schumy <ksschumy@amazon.com>

---
+++ cmd/release/utils/values/searchprs.go
@@ -0,0 +1,62 @@
+package values
+
+import (
+	"fmt"
+	"strings"
+	"context"
+
+	"github.com/google/go-github/v47/github"
+)
+
+const (
+	baseQuery = "repo:aws/eks-distro is:pr is:merged"
+)
+
+func GetChangelogPRs(releaseVersion string) (string, error) {
+	githubClient := github.NewClient(nil)
+
+	ctx := context.Background()
+	opts := &github.SearchOptions{}
+	prs, _, err := githubClient.Search.Issues(ctx, "is:pr label:documentation repo:aws/eks-distro label:" + releaseVersion, opts)
+	if err != nil {
+		return "", fmt.Errorf("Getting PRs from %v: %v", githubClient, err)
+	}
+
+	lastDocRelease := prs.Issues[0].ClosedAt.Format("2006-01-02T15:04:05+00:00")
+	
+	patchPRs, _, err := githubClient.Search.Issues(ctx, fmt.Sprintf("%v merged:>%v label:patch label:%v", baseQuery, lastDocRelease, releaseVersion), opts)
+	if err != nil {
+		return "", fmt.Errorf("Getting patch prs: %v", err)
+	}
+
+	baseImgPRs, _, err := githubClient.Search.Issues(ctx, fmt.Sprintf("%v merged:>%v label:base-img-pkg-update label:%v",baseQuery, lastDocRelease, releaseVersion), opts)
+	if err != nil {
+		return "", fmt.Errorf("Getting base image prs: %v", err)
+	}
+
+	versPRs, _, err := githubClient.Search.Issues(ctx, fmt.Sprintf("%v merged:>%v label:project label:%v",baseQuery, lastDocRelease, releaseVersion), opts)
+	if err != nil {
+		return "", fmt.Errorf("Getting project prs: %v", err)
+	}
+
+	var changelog []string
+	changelog = append(changelog, PRsSinceLastRelease(patchPRs, "### Patches"))
+	changelog = append(changelog, PRsSinceLastRelease(versPRs, "### Projects"))
+	changelog = append(changelog, PRsSinceLastRelease(baseImgPRs, "### Base Image"))
+
+	return strings.Join(changelog, "\n"), nil
+}
+
+func PRsSinceLastRelease(prs *github.IssuesSearchResult, sectionName string) (string) {
+	var section []string
+	section = append(section, sectionName)
+
+	if len(prs.Issues) == 0 {
+		section = append(section, "No changes since last release")
+	}
+
+	for _, pr := range prs.Issues {
+		section = append(section, fmt.Sprintf("* %v ([%v](%v))", *pr.Title, *pr.ID, *pr.URL))
+	}
+	return strings.Join(section, "\n")
+}
