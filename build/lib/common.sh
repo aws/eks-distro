9afbcbc239c22610456b929912911201a25f8fdd
Jackson West <jgw@amazon.com>
2022-11-29 10:39:58 -0600

    Fixes windows manifest validation

--- build/lib/common.sh
+++ build/lib/common.sh
@@ -295,7 +295,7 @@ function retry() {
     "$@" && break || {
       if [[ $n -lt $max ]]; then
         ((n++))
-        echo "Command failed. Attempt $n/$max:"
+        >&2 echo "Command failed. Attempt $n/$max:"
         sleep $delay;
       else
         fail "The command has failed after $n attempts."
