3d5afdf2225b2bd792d35956c92d4ca95ddef6be
Jackson West <jgw@amazon.com>
2022-12-19 09:40:04 -0600

    Regenerated help.mk files

--- projects/coredns/coredns/Help.mk
+++ projects/coredns/coredns/Help.mk
@@ -25,6 +25,18 @@ coredns/images/arm64: ## Builds/pushes `coredns/images/arm64`
 ##@ Checksum Targets
 checksums: ## Update checksums file based on currently built binaries.
 validate-checksums: # Validate checksums of currently built binaries against checksums file.
+all-checksums: ## Update checksums files for all RELEASE_BRANCHes.
+
+##@ Run in Docker Targets
+run-all-attributions-in-docker: ## Run `all-attributions` in docker builder container
+run-all-attributions-checksums-in-docker: ## Run `all-attributions-checksums` in docker builder container
+run-all-checksums-in-docker: ## Run `all-checksums` in docker builder container
+run-attribution-in-docker: ## Run `attribution` in docker builder container
+run-attribution-checksums-in-docker: ## Run `attribution-checksums` in docker builder container
+run-binaries-in-docker: ## Run `binaries` in docker builder container
+run-checksums-in-docker: ## Run `checksums` in docker builder container
+run-clean-in-docker: ## Run `clean` in docker builder container
+run-clean-go-cache-in-docker: ## Run `clean-go-cache` in docker builder container
 
 ##@ Artifact Targets
 tarballs: ## Create tarballs by calling build/lib/simple_create_tarballs.sh unless SIMPLE_CREATE_TARBALLS=false, then tarballs must be defined in project Makefile
@@ -35,6 +47,9 @@ upload-artifacts: # Upload tarballs and other artifacts from ARTIFACTS_PATH to S
 gather-licenses: ## Helper to call $(GATHER_LICENSES_TARGETS) which gathers all licenses
 attribution: ## Generates attribution from licenses gathered during `gather-licenses`.
 attribution-pr: ## Generates PR to update attribution files for projects
+attribution-checksums: ## Update attribution and checksums files.
+all-attributions: ## Update attribution files for all RELEASE_BRANCHes.
+all-attributions-checksums: ## Update attribution and checksums files for all RELEASE_BRANCHes.
 
 ##@ Clean Targets
 clean: ## Removes source and _output directory
@@ -46,7 +61,6 @@ add-generated-help-block: ## Add or update generated help block to document proj
 
 ##@Update Helpers
 run-target-in-docker: ## Run `MAKE_TARGET` using builder base docker container
-update-attribution-checksums-docker: ## Update attribution and checksums using the builder base docker container
 stop-docker-builder: ## Clean up builder base docker container
 generate: ## Update UPSTREAM_PROJECTS.yaml
 update-go-mods: ## Update locally checked-in go sum to assist in vuln scanning
