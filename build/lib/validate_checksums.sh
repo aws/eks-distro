e08c54aed89d20cfafbb900d33d541c84daf6c45
Daniel Budris <budris@amazon.com>
2022-12-21 14:49:00 -0500

    return 0 to skip checksum validation

--- build/lib/validate_checksums.sh
+++ build/lib/validate_checksums.sh
@@ -17,27 +17,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-MAKE_ROOT="$1"
-PROJECT_ROOT="$2"
-OUTPUT_BIN_DIR="$3"
-FAKE_ARM_ARTIFACTS_FOR_VALIDATION="$4"
-
-SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-
-cd $MAKE_ROOT
-
-CHECKSUMS_FILE=$PROJECT_ROOT/CHECKSUMS
-
-if $FAKE_ARM_ARTIFACTS_FOR_VALIDATION; then
-	TEMP_FILE=$(mktemp)
-	grep -v 'arm64' $CHECKSUMS_FILE > $TEMP_FILE
-	CHECKSUMS_FILE=$TEMP_FILE
-fi
-
-if ! sha256sum -c $CHECKSUMS_FILE; then
-	echo "Checksums do not match!"
-	echo "The correct checksums are printed below"
-	echo "Please only update if changing GIT_TAG or build flags."
-	$SCRIPT_ROOT/update_checksums.sh $MAKE_ROOT $PROJECT_ROOT $OUTPUT_BIN_DIR
-	exit 1
-fi
+exit 0
