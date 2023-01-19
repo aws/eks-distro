ae84411f6e967e547cca18c2ec6d4ca8ccfed5cb
Kirsten Schumy <ksschumy@amazon.com>
2023-01-19 09:45:55 -0800

    Update projects/kubernetes/kubernetes/build/lib/binaries.sh
    Co-authored-by: Mark Pruett <67116121+markapruett@users.noreply.github.com>

--- projects/kubernetes/kubernetes/build/lib/binaries.sh
+++ projects/kubernetes/kubernetes/build/lib/binaries.sh
@@ -49,11 +49,6 @@ function build::binaries::kube_bins() {
         cmd/kube-controller-manager \
         cmd/kube-scheduler"
 
-    if [ "$release_branch" == "1-26" ]; then
-      hack/update-codegen.sh
-    else
-        make generated_files
-    fi
 
     # Linux
     export KUBE_BUILD_PLATFORMS="linux/amd64 linux/arm64"
