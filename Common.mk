a2e40279c46561dbdb98f9a90ef6e0bf5f5af58c
Cameron Rozean <rcrozean@amazon.com>
2023-02-27 15:45:32 -0800

    tested in dev desktop

--- Common.mk
+++ Common.mk
@@ -921,7 +921,7 @@ update-go-mods: checkout-repo
 	for gomod in $(GO_MOD_PATHS); do \
 		mkdir -p $(DEST_PATH); \
 		cp $(REPO)/$$gomod/go.{mod,sum} $(DEST_PATH); \
-		sed -i $(DEST_PATH)/go.mod "s,go 1.*,go 1.19,"; \
+		sed -i -e "s,go 1.*,go 1.19," $(DEST_PATH)/go.mod; \
 		go mod tidy; \
 	done
 
