ab5826d50e2f3d555506cfebc438c3db2b498a36
Kirsten Schumy <ksschumy@amazon.com>
2023-07-17 12:10:24 -0700

    Updated kOps to 1.27

--- development/kops/set_environment.sh
+++ development/kops/set_environment.sh
@@ -24,7 +24,7 @@ export NODE_INSTANCE_TYPE=${NODE_INSTANCE_TYPE:-t3.medium}
 export NODE_ARCHITECTURE=${NODE_ARCHITECTURE:-amd64}
 export UBUNTU_RELEASE=${UBUNTU_RELEASE:-focal-20.04}
 export IPV6=${IPV6:-false}
-export KOPS_VERSION="1.26.3"
+export KOPS_VERSION="1.27.0"
 
 if [ -n "$ARTIFACT_BUCKET" ]; then
     export ARTIFACT_BASE_URL="https://$ARTIFACT_BUCKET.s3.amazonaws.com"
