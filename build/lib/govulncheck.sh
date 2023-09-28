597aca58aa7f5a68b46921b93405a08a8f5e0920
Daniel Budris <budris@amazon.com>
2023-09-28 09:39:10 -0700

    add a modcache clean

--- build/lib/govulncheck.sh
+++ build/lib/govulncheck.sh
@@ -27,6 +27,7 @@ rungovulncheck() {
     build::common::use_go_version $goversion
     go version
     go install golang.org/x/vuln/cmd/govulncheck@latest
+    go clean -modcache
     govluncheckoutput=$($(go env GOPATH)/bin/govulncheck -C $repo -json ./...)
     echo $govluncheckoutput
     detectedcves=$(echo $govluncheckoutput | jq '.osv | select( . != null ) | .aliases[0]')
