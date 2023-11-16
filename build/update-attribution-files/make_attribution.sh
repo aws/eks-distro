4a37bfb0366aca873b14b98cdc3d094b79109791
Sajia Zafreen <sajiazafreen@u.boisestate.edu>
2023-11-16 10:36:11 -0800

    update if check

--- build/update-attribution-files/make_attribution.sh
+++ build/update-attribution-files/make_attribution.sh
@@ -56,26 +56,24 @@ else
         GIT_TAG="$(cat $PROJECT_ROOT/$release/GIT_TAG)"
 
         if [[ $release != "1-23" ]] || [[ $PROJECT == "kubernetes" ]]; then
-            if [ "$GIT_TAG" != "$LAST_GIT_TAG" ] || [ $TARGET == "update-go-mods" ]; then
+            if [[ $LAST_RELEASE_BRANCH == "1-23" ]] || [ "$GIT_TAG" != "$LAST_GIT_TAG" ] || [ $TARGET == "update-go-mods" ]; then
                 # clean before regenerating to ensure there are no intermediate files left around
                 make -C $PROJECT_ROOT clean clean-go-cache
                 build::attribution::generate $release
             else
                 # if the git_tags match across release branches, save the output state to avoid
                 # rebuilding/regenerating
-                if [[ $LAST_RELEASE_BRANCH != "1-23" ]]; then
-                    if [[ $TARGET == *"checksums"* ]]; then
-                        echo "Copying $LAST_RELEASE_BRANCH CHECKSUMS to $release"
-                        mkdir -p $PROJECT_ROOT/_output/$release
-                        sed "s/$LAST_RELEASE_BRANCH/$release/" $PROJECT_ROOT/$LAST_RELEASE_BRANCH/CHECKSUMS > $PROJECT_ROOT/$release/CHECKSUMS
-                    fi
+                if [[ $TARGET == *"checksums"* ]]; then
+                    echo "Copying $LAST_RELEASE_BRANCH CHECKSUMS to $release"
+                    mkdir -p $PROJECT_ROOT/_output/$release
+                    sed "s/$LAST_RELEASE_BRANCH/$release/" $PROJECT_ROOT/$LAST_RELEASE_BRANCH/CHECKSUMS > $PROJECT_ROOT/$release/CHECKSUMS
+                fi
 
-                    if [[ $TARGET == *"attribution"* ]]; then
-                        echo "Copying $LAST_RELEASE_BRANCH ATTRIBUTION to $release"
-                        mkdir -p $PROJECT_ROOT/_output/$release
-                        cp -rf $PROJECT_ROOT/$LAST_RELEASE_BRANCH/*TTRIBUTION.txt $PROJECT_ROOT/$release
-                    fi 
-                fi           
+                if [[ $TARGET == *"attribution"* ]]; then
+                    echo "Copying $LAST_RELEASE_BRANCH ATTRIBUTION to $release"
+                    mkdir -p $PROJECT_ROOT/_output/$release
+                    cp -rf $PROJECT_ROOT/$LAST_RELEASE_BRANCH/*TTRIBUTION.txt $PROJECT_ROOT/$release
+                fi         
             fi
         fi
 
