78f4e7c35fe7fa608fc5ae41e5133aa070220a69
Cameron Rozean <rcrozean@amazon.com>
2023-11-21 14:23:32 -0800

    bump kops for 1.29 (#2558)
    Signed-off-by: Cameron Rozean <rcrozean@amazon.com>

--- development/kops/set_environment.sh
+++ development/kops/set_environment.sh
@@ -24,7 +24,9 @@ export NODE_INSTANCE_TYPE=${NODE_INSTANCE_TYPE:-t3.medium}
 export NODE_ARCHITECTURE=${NODE_ARCHITECTURE:-amd64}
 export UBUNTU_RELEASE=${UBUNTU_RELEASE:-focal-20.04}
 export IPV6=${IPV6:-false}
-if [ "$RELEASE_BRANCH" == "1-28" ]; then
+if [ "$RELEASE_BRANCH" == "1-29" ]; then
+	export KOPS_VERSION="1.29.0-alpha.2"
+elif [ "$RELEASE_BRANCH" == "1-28" ]; then
 	export KOPS_VERSION="1.28.0-alpha.1"
 else
 	export KOPS_VERSION="1.27.0"
