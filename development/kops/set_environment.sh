1bb185fcc9f169fda85673ca137241a6b5803a14
Bhavitha <106284071+bhavi-koduru@users.noreply.github.com>
2024-04-10 17:44:16 -0700

    Fix kops version (#2907)

--- development/kops/set_environment.sh
+++ development/kops/set_environment.sh
@@ -25,7 +25,7 @@ export NODE_ARCHITECTURE=${NODE_ARCHITECTURE:-amd64}
 export UBUNTU_RELEASE=${UBUNTU_RELEASE:-focal-20.04}
 export IPV6=${IPV6:-false}
 if [[ ! "$RELEASE_BRANCH" < "1-29" ]]; then
-	export KOPS_VERSION="v1.29.0-beta.1"
+	export KOPS_VERSION="1.29.0-beta.1"
 else
 	export KOPS_VERSION="1.28.4"
 fi
