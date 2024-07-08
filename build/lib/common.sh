318d4f991729c32b461f3cd1cb8e9b4559fccb01
Cameron Rozean <rcrozean@amazon.com>
2024-07-08 10:08:06 -0700

    DO NOT MERGE. This is whitespace change to test builder-base changes

--- build/lib/common.sh
+++ build/lib/common.sh
@@ -13,11 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 if [ -n "${OUTPUT_DEBUG_LOG:-}" ]; then
-    set -x
+  set -x
 fi
-
 
 function build::common::ensure_tar() {
   if [[ -n "${TAR:-}" ]]; then
@@ -27,9 +25,9 @@ function build::common::ensure_tar() {
   # Find gnu tar if it is available, bomb out if not.
   TAR=tar
   if which gtar &>/dev/null; then
-      TAR=gtar
+    TAR=gtar
   elif which gnutar &>/dev/null; then
-      TAR=gnutar
+    TAR=gnutar
   fi
   if ! "${TAR}" --version | grep -q GNU; then
     echo "  !!! Cannot find GNU tar. Build on Linux or install GNU tar"
@@ -66,12 +64,11 @@ function build::common::generate_shasum() {
   cd $tarpath
   for file in $(find . -name '*.tar.gz'); do
     filepath=$(basename $file)
-    sha256sum "$filepath" > "$file.sha256"
-    sha512sum "$filepath" > "$file.sha512"
+    sha256sum "$filepath" >"$file.sha256"
+    sha512sum "$filepath" >"$file.sha512"
   done
   cd -
 }
-
 
 function build::gather_licenses() {
   local -r outputdir=$1
@@ -83,14 +80,13 @@ function build::gather_licenses() {
   # have to be manually created
   export GOFLAGS=-mod=vendor
   # force platform to be linux to ensure all deps are picked up
-  export GOOS=linux 
-  export GOARCH=amd64 
+  export GOOS=linux
+  export GOARCH=amd64
 
   # the version of go used here must be the version go-licenses was installed with corresponding go versions
   build::common::use_go_version $golang_version
 
-  if ! command -v go-licenses &> /dev/null
-  then
+  if ! command -v go-licenses &>/dev/null; then
     echo " go-licenses not found.  If you need license or attribution file handling"
     echo " please refer to the doc in docs/development/attribution-files.md"
     exit
@@ -101,23 +97,23 @@ function build::gather_licenses() {
   # data about each dependency to generate the amazon approved attribution.txt files
   # go-deps is needed for module versions
   # go-licenses are all the dependencies found from the module(s) that were passed in via patterns
-  build::common::echo_and_run go list -deps=true -json ./... | jq -s '.'  > "${outputdir}/attribution/go-deps.json"
+  build::common::echo_and_run go list -deps=true -json ./... | jq -s '.' >"${outputdir}/attribution/go-deps.json"
 
-  # go-licenses can be a bit noisy with its output and lot of it can be confusing 
+  # go-licenses can be a bit noisy with its output and lot of it can be confusing
   # the following messages are safe to ignore since we do not need the license url for our process
   NOISY_MESSAGES="cannot determine URL for|Error discovering license URL|unsupported package host|contains non-Go code|has empty version|vendor.*\.s$"
 
   build::common::echo_and_run go-licenses save --force $patterns --save_path "${outputdir}/LICENSES" 2> >(grep -vE "$NOISY_MESSAGES")
-  
-  build::common::echo_and_run go-licenses csv $patterns 2> >(grep -vE "$NOISY_MESSAGES") > "${outputdir}/attribution/go-license.csv"  
+
+  build::common::echo_and_run go-licenses csv $patterns 2> >(grep -vE "$NOISY_MESSAGES") >"${outputdir}/attribution/go-license.csv"
 
   if cat "${outputdir}/attribution/go-license.csv" | grep -q "^vendor\/golang.org\/x"; then
-      echo " go-licenses created a file with a std golang package (golang.org/x/*)"
-      echo " prefixed with vendor/.  This most likely will result in an error"
-      echo " when generating the attribution file and is probably due to"
-      echo " to a version mismatch between the current version of go "
-      echo " and the version of go that was used to build go-licenses"
-      exit 1
+    echo " go-licenses created a file with a std golang package (golang.org/x/*)"
+    echo " prefixed with vendor/.  This most likely will result in an error"
+    echo " when generating the attribution file and is probably due to"
+    echo " to a version mismatch between the current version of go "
+    echo " and the version of go that was used to build go-licenses"
+    exit 1
   fi
 
   if cat "${outputdir}/attribution/go-license.csv" | grep -e ",LGPL-" -e ",GPL-"; then
@@ -142,16 +138,16 @@ function build::gather_licenses() {
   # from the go.mod file, storing that away since the source dirs usually get deleted
   MODULE_NAME=$(go mod edit -json | jq -r '.Module.Path')
   if [ ! -f ${outputdir}/attribution/root-module.txt ]; then
-  	echo $MODULE_NAME > ${outputdir}/attribution/root-module.txt
+    echo $MODULE_NAME >${outputdir}/attribution/root-module.txt
   fi
 }
 
-function build::non-golang::gather_licenses(){
+function build::non-golang::gather_licenses() {
   local -r project="$1"
   local -r git_tag="$2"
   local -r output_dir="$3"
-  project_org="$(cut -d '/' -f1 <<< ${project})"
-  project_name="$(cut -d '/' -f2 <<< ${project})"
+  project_org="$(cut -d '/' -f1 <<<${project})"
+  project_name="$(cut -d '/' -f2 <<<${project})"
   git clone https://github.com/${project_org}/${project_name}
   cd $project_name
   git checkout $git_tag
@@ -160,19 +156,21 @@ function build::non-golang::gather_licenses(){
   rm -rf $project_name
 }
 
-function build::non-golang::copy_licenses(){
+function build::non-golang::copy_licenses() {
   local -r source_dir="$1"
   local -r destination_dir="$2"
-  (cd $source_dir; find . \( -name "*COPYING*" -o -name "*COPYRIGHT*" -o -name "*LICEN[C|S]E*" -o -name "*NOTICE*" \)) |
-  while read file
-  do
-    license_dest=$destination_dir/$(dirname $file)
-    mkdir -p $license_dest
-    cp -r "${source_dir}/${file}" $license_dest/$(basename $file)
-  done
+  (
+    cd $source_dir
+    find . \( -name "*COPYING*" -o -name "*COPYRIGHT*" -o -name "*LICEN[C|S]E*" -o -name "*NOTICE*" \)
+  ) \
+    | while read file; do
+      license_dest=$destination_dir/$(dirname $file)
+      mkdir -p $license_dest
+      cp -r "${source_dir}/${file}" $license_dest/$(basename $file)
+    done
 }
 
-function build::generate_attribution(){
+function build::generate_attribution() {
   local -r project_root=$1
   local -r golang_version=$2
   local -r output_directory=${3:-"${project_root}/_output"}
@@ -189,7 +187,7 @@ function build::generate_attribution(){
     exit 1
   fi
 
-  build::common::echo_and_run generate-attribution $root_module_name $project_root $golang_version_tag $output_directory 
+  build::common::echo_and_run generate-attribution $root_module_name $project_root $golang_version_tag $output_directory
   cp -f "${output_directory}/attribution/ATTRIBUTION.txt" $attribution_file
 }
 
@@ -215,7 +213,7 @@ function build::common::get_go_path() {
 function build::common::use_go_version() {
   local -r version=$1
 
-  if (( "${version#*.}" < 16 )); then
+  if (("${version#*.}" < 16)); then
     echo "Building with GO version $version is no longer supported!  Please update the build to use a newer version."
     exit 1
   fi
@@ -238,8 +236,8 @@ function build::common::set_go_cache() {
 }
 
 function build::common::re_quote() {
-    local -r to_escape=$1
-    sed 's/[][()\.^$\/?*+]/\\&/g' <<< "$to_escape"
+  local -r to_escape=$1
+  sed 's/[][()\.^$\/?*+]/\\&/g' <<<"$to_escape"
 }
 function build::common::wait_for_tag() {
   local -r tag=$1
@@ -247,7 +245,7 @@ function build::common::wait_for_tag() {
   for i in {1..60}; do
     echo "Checking for tag ${tag}..."
     git rev-parse --verify --quiet "${tag}" && echo "Tag ${tag} exists!" && break
-    git fetch --tags > /dev/null 2>&1
+    git fetch --tags >/dev/null 2>&1
     echo "Tag ${tag} does not exist!"
     echo "Waiting for tag ${tag}..."
     sleep $sleep_interval
@@ -263,7 +261,7 @@ function build::common::wait_for_tarball() {
   for i in {1..60}; do
     echo "Checking for URL ${tarball_url}..."
     local -r http_code=$(curl -I -L -s -o /dev/null -w "%{http_code}" $tarball_url)
-    if [[ "$http_code" == "200" ]]; then 
+    if [[ "$http_code" == "200" ]]; then
       echo "Tarball exists!" && break
     fi
     echo "Tarball does not exist!"
@@ -292,7 +290,7 @@ function retry() {
       if [[ $n -lt $max ]]; then
         ((n++))
         >&2 echo "Command failed. Attempt $n/$max:"
-        sleep $delay;
+        sleep $delay
       else
         fail "The command has failed after $n attempts."
       fi
@@ -308,3 +306,4 @@ function build::common::echo_and_run() {
   >&2 echo "($(pwd)) \$ $*"
   "$@"
 }
+
