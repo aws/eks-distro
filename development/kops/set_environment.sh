dc573b3fcb174de821d4c7be5254922ff8ef8cef
Varma Maryala <mpvarma@amazon.com>
2024-08-09 13:11:31 -0700

    updated the kops git tag to 1.29.2

--- development/kops/set_environment.sh
+++ development/kops/set_environment.sh
@@ -25,11 +25,7 @@ export NODE_ARCHITECTURE=${NODE_ARCHITECTURE:-amd64}
 export UBUNTU_RELEASE=${UBUNTU_RELEASE:-focal-20.04}
 export IPV6=${IPV6:-false}
 
-if [[ ! "$RELEASE_BRANCH" < "1-29" ]]; then
-  export KOPS_VERSION="1.29.0-beta.1"
-else
-  export KOPS_VERSION="1.28.1"
-fi
+export KOPS_VERSION="1.29.2"
 
 # Check for the occasion the release branch version is newer than the most recent release of KOPS
 if [[ ${RELEASE_BRANCH#*-} > ${KOPS_VERSION#*.} ]]; then
