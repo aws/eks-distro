b317e5ac50d37d18bd2e08c24a577ddbdae79591
Vineeth Bandi <vineeth.bandi@utexas.edu>
2026-01-14 11:39:52 -0600

    move etcd and cloud-provider-aws to internal builds (#4434)

---
+++ projects/etcd-io/etcd/build/bootstrap_build_variables.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -eux
+
+TEMP_FILE=$(mktemp)
+trap 'rm -f $TEMP_FILE' EXIT
+S3_BUCKET="eks-distro-dev-release-artifacts"
+S3_PREFIX="kubernetes-${RELEASE_BRANCH}/releases/"
+aws s3 ls --recursive "s3://${S3_BUCKET}/${S3_PREFIX}" > "$TEMP_FILE"
+
+LATEST_VERSION=$(grep -o 'eks\.[0-9]\+' "$TEMP_FILE" | sort -V | tail -1)
+S3_PATH="${S3_PREFIX}${LATEST_VERSION}/artifacts/etcd"
+
+aws s3 cp "s3://${S3_BUCKET}/${S3_PATH}/.git_tag" .git_tag
+aws s3 cp "s3://${S3_BUCKET}/${S3_PATH}/.go-version" /tmp/.go-version
+
+GOLANG_VERSION=$(cut -d. -f1,2 < /tmp/.go-version)
+MIN_GO_VERSION="1.24"
+if [[ "$(printf '%s\n' "$MIN_GO_VERSION" "$GOLANG_VERSION" | sort -V | head -1)" != "$MIN_GO_VERSION" ]]; then
+  GOLANG_VERSION="$MIN_GO_VERSION"
+fi
+echo "$GOLANG_VERSION" > .go-version
+
+GIT_TAG=$(cat .git_tag)
+MIN_VERSION="v3.5.21"
+if [[ "$(printf '%s\n' "$MIN_VERSION" "$GIT_TAG" | sort -V | head -1)" != "$MIN_VERSION" ]]; then
+  GIT_TAG="$MIN_VERSION"
+  echo "$GIT_TAG" > .git_tag
+fi
+curl -sL "https://raw.githubusercontent.com/etcd-io/etcd/${GIT_TAG}/go.mod" -o "${RELEASE_BRANCH}/go.mod"
+curl -sL "https://raw.githubusercontent.com/etcd-io/etcd/${GIT_TAG}/go.sum" -o "${RELEASE_BRANCH}/go.sum"
---
+++ projects/kubernetes/cloud-provider-aws/build/bootstrap_copy_release_variables.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -eux
+
+TEMP_FILE=$(mktemp)
+trap 'rm -f $TEMP_FILE' EXIT
+ARTIFACTS_SOURCE_S3_BUCKET="eks-distro-dev-release-artifacts"
+S3_PREFIX="kubernetes-${RELEASE_BRANCH}/releases/"
+aws s3 ls --recursive "s3://${ARTIFACTS_SOURCE_S3_BUCKET}/${S3_PREFIX}" > "$TEMP_FILE"
+
+LATEST_VERSION=$(grep -o 'eks\.[0-9]\+' "$TEMP_FILE" | sort -V | tail -1)
+
+VERSION_DIR=$(grep "${LATEST_VERSION}/artifacts/${REPO}/v[0-9]" "$TEMP_FILE" | \
+              grep -o 'v[0-9][^/]*' | head -1)
+
+GIT_TAG=$(echo "${VERSION_DIR}" | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+')
+echo "${GIT_TAG}" > .git_tag
+echo "${VERSION_DIR}" > .source_image_tag
+
+ARTIFACTS_SOURCE_S3_PATH="s3://${ARTIFACTS_SOURCE_S3_BUCKET}/${S3_PREFIX}${LATEST_VERSION}/artifacts/${REPO}/${VERSION_DIR}/"
+echo "${ARTIFACTS_SOURCE_S3_PATH}" > .s3_sync_path
+
+aws s3 cp "${ARTIFACTS_SOURCE_S3_PATH}.go-version" .go-version
